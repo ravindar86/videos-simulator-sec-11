59e6dea9a780d066624e3d2467f7e4fea34fe88d
ravindar86 <ravindar.mca@gmail.com>
2021-12-27 17:38:36 +0530

    Commit for YouTube player simulation

---
+++ src/api/youtube.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const KEY='AIzaSyA31z3KGq5iaN8FOFxfqo8PXNh076s4eRc';
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY,
+        type: 'video'
+    }
+});
